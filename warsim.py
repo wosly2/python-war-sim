6fa3a3d62e5e9cbd72a0cafe4a5294d9e651b997
sawyer jackson <sawyergjackson@gmail.com>
2024-09-12 15:44:47 -0400

    added more descriptive documentation and program output

--- warsim.py
+++ warsim.py
@@ -2,16 +2,17 @@ import random as rd
 
 
 # deck defs
-cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace",]
+cards: list[str] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace",]
 
 # creating the deck
-deck = []
+deck: list[str] = []
 for card in cards:
     for i in range(4): # one for each suit
         deck.append(card)
 
 # func to find the higher card
 def return_winner(card_1:str, card_2:str, two_beats_ace:bool=True) -> int:
+    """Give two strings from list `cards` and return higher card or tie as int in 1, 2, 0."""
     card_1_val = cards.index(card_1)
     card_2_val = cards.index(card_2)
     if two_beats_ace and ((card_1_val == 0 and card_2_val == 51) or (card_1_val == 51 and card_2_val == 0)):
@@ -29,9 +30,10 @@ def return_winner(card_1:str, card_2:str, two_beats_ace:bool=True) -> int:
     return 0
 
 # splitting into two decks
-def make_two_decks() -> tuple:
-    deck_1 = []
-    deck_2 = []
+def make_two_decks() -> tuple[list[str], list[str]]:
+    """Create two random decks from a master deck (their sum being the provided deck)."""
+    deck_1: list[str] = []
+    deck_2: list[str] = []
     remaining_indices = list(range(len(deck)))
     while len(remaining_indices) > 0:
         for i in range(2):
@@ -46,6 +48,14 @@ def make_two_decks() -> tuple:
 
 # simulate func
 def simulate(two_beats_ace:bool=True, max_its:int=10000, verbose:bool=True, print_whole_deck:bool=False) -> None:
+    """Simulate a game of war between two players.
+
+    Keyword Arguments:
+        two_beats_ace -- boolean, whether or not a two will be considered higher than an ace (default True)
+        max_its -- int, maximum simulation iteration (default 10000)
+        verbose -- bool, wether the program will print certain extra information to the terminal, does not affect final winner output (default True)
+        print_whole_deck -- bool, whether the program will print the whole deck of either player each iteration when verbose == True (default False)
+    """
     deck_1, deck_2 = make_two_decks()
     iterations = 0
     while len(deck_1) > 0 and len(deck_2) > 0 and iterations <= max_its:
@@ -54,6 +64,8 @@ def simulate(two_beats_ace:bool=True, max_its:int=10000, verbose:bool=True, prin
             if print_whole_deck:
                 print("Player 1 deck:", deck_1)
                 print("Player 2 deck:", deck_2)
+            print("Player 1 has", len(deck_1), "cards")
+            print("Player 2 has", len(deck_2), "cards")
             print("Player 1 card:", deck_1[0])
             print("Player 2 card:", deck_2[0])
 
@@ -83,8 +95,13 @@ def simulate(two_beats_ace:bool=True, max_its:int=10000, verbose:bool=True, prin
                 if verbose:
                     print("WAR Initiated")
                     print("Winnings at stake:", war_winnings)
-
+        if verbose:
+            print("\n")
         iterations += 1
+    if len(deck_1) == 0:
+        print("Player 2 won the game!")
+    elif len(deck_2) == 0:
+        print("Player 1 won the game!")
 
 if __name__ == "__main__":
     simulate()
